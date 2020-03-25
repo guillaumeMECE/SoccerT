3adcf22bb7b7eef2b6222e6f9488fae6cea4ee91
Guillaume <guillaume.maurin@edu.ece.fr>
2020-03-25 23:00:10 +0100

    Add card to result frag

--- app/src/main/java/com/ece/soccert/ui/results/ResultsFragment.java
+++ app/src/main/java/com/ece/soccert/ui/results/ResultsFragment.java
@@ -1,4 +1,4 @@
-package com.ece.soccert.ui.home;
+package com.ece.soccert.ui.results;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.ece.soccert.R;
 
-public class HomeFragment extends Fragment {
+public class ResultsFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private ResultsViewModel resultsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        resultsViewModel =
+                ViewModelProviders.of(this).get(ResultsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_results, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        resultsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/ece/soccert/ui/results/ResultsViewModel.java
+++ app/src/main/java/com/ece/soccert/ui/results/ResultsViewModel.java
@@ -1,14 +1,14 @@
-package com.ece.soccert.ui.home;
+package com.ece.soccert.ui.results;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class ResultsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public ResultsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
     }
