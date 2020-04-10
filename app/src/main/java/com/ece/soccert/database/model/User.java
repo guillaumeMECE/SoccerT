db80ce20b80eedb6f29eccaea723cdf680c272af
ClaraBzk <38009877+ClaraBzk@users.noreply.github.com>
2020-04-10 20:05:36 +0200

    User creation

---
+++ app/src/main/java/com/ece/soccert/database/model/User.java
@@ -0,0 +1,84 @@
+package com.ece.soccert.database.model;
+
+import android.media.Image;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.widget.ImageView;
+
+import java.sql.Blob;
+
+
+public class User {
+
+    public static final String TABLE_NAME = "user";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_SURNAME = "surname";
+    public static final String COLUMN_PHOTO = "photo";
+
+    private int id;
+    private String name;
+    private String surname;
+    private byte[] photo;
+
+    // Create table SQL query
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + COLUMN_NAME + " TEXT,"
+                    + COLUMN_SURNAME + " TEXT,"
+                    + COLUMN_PHOTO + " IMAGE ,"
+                    + ")";
+
+   /* public User(int anInt, String string, String cursorString, byte[] blob) {
+    }*/
+
+
+    public User(int id, String name, String surname, byte[] photo) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.photo = photo;
+    }
+
+    //GET
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+
+    //SET
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+
+
+
+}
+
--- app/src/main/java/com/ece/soccert/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/ece/soccert/ui/notifications/NotificationsFragment.java
@@ -1,35 +1,182 @@
 package com.ece.soccert.ui.notifications;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.ContentResolver;
+import android.content.Context;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ece.soccert.R;
+import com.ece.soccert.database.DatabaseHelper;
+import com.ece.soccert.view.MainActivity;
+
+import java.io.FileNotFoundException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static android.app.Activity.RESULT_OK;
 
 public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    ImageView photo;
+    Button buttonChoose;
+    Button buttonAdd;
+    EditText name;
+    EditText surname;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+    final int REQUEST_CODE_GALLERY = 999;
+
+    public static DatabaseHelper db;
+
+
+    private NotificationsViewModel mViewModel;
+
+    public static NotificationsFragment newInstance() {
+        return new NotificationsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        buttonChoose = (Button) root.findViewById(R.id.choose_photo);
+        buttonAdd = (Button) root.findViewById(R.id.add_user);
+        photo = (ImageView) root.findViewById(R.id.photo);
+        name = (EditText) root.findViewById(R.id.name);
+        surname = (EditText) root.findViewById(R.id.surname);
+
+        db = new DatabaseHelper(getActivity());
+
+        buttonChoose.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY
+                );
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 101);
+            }
+
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    db.insertUser(
+                            name.getText().toString(),
+                            surname.getText().toString(),
+                            imageViewToByte(photo)
+                    );
+                    Toast.makeText(getActivity().getApplicationContext(), "Added successfully !", Toast.LENGTH_SHORT).show();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
             }
         });
+
+
         return root;
+
+
+    }
+
+    public static byte[] imageViewToByte(ImageView photo) {
+        Bitmap bitmap = ((BitmapDrawable) photo.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_CODE_GALLERY) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
+            } else {
+                Toast.makeText(getActivity().getApplicationContext(), "You don't have permission to access file location!", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            if (resultCode == getActivity().RESULT_OK) {
+                if (data != null) {
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
+                        photo.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (resultCode == getActivity().RESULT_CANCELED) {
+                Toast.makeText(getActivity(), "Canceled", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
